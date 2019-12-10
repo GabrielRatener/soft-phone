4baeec42ac16fe73b43fb74ab700c6132b0c2a22
Gabriel Ratener <gabe9693@gmail.com>
2019-12-09 23:13:38 -0800

    Switch to webpack

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,11 +1,19 @@
 
 import React from "react"
-import {Button} from "@material-ui/core"
+import Toolbar from "@material-ui/core/toolbar"
+import AppBar from "@material-ui/core/appbar"
+import Button from "@material-ui/core/button"
+import Grid from "@material-ui/core/grid"
 
 export default () => {
     return (
         <div>
-            <Button>Hello Tom!</Button>
+            <AppBar position="static">
+                <Toolbar>
+                    <Button>Call</Button>
+                    <Button>SMS</Button>
+                </Toolbar>
+            </AppBar>
         </div>
     );
 }
